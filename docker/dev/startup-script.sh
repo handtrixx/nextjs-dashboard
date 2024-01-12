97e55147a1ce59d2feb4fb175e69eb892e04584c
handtrixxx <pooh85@gmx.de>
2024-01-12 17:19:16 +0100

    docker dev

--- docker/dev/startup-script.sh
+++ docker/dev/startup-script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-rm -R /usr/src/app/node_modules/
 
-# Start your Node.js application
+echo Deleting installed node modules for refresh if existing
+rm -R /usr/src/app/node_modules/
+echo Installing node modules from package.json
 npm install --no-progress
+echo Starting your Node.js application
 npm run dev 
 
 # Keep the script running in case of errors to be able to inspect the container
